318a93af037d68500412772827a3b72f4c812c4a
BryanTorres18 <90003001+BryanTorres18@users.noreply.github.com>
2025-07-23 14:46:34 -0500

    Prueba del despliegue 2

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 export default {
 	async fetch(request, env, ctx): Promise<Response> {
-		return new Response('Hola soy Bryan Torres');
+		return new Response('Hola soy Bryan');
 	},
 } satisfies ExportedHandler<Env>;
